7cffca9de06f43754857de7683be8d065fd57bec
zakzak25 <zbousba4@gmail.com>
2024-06-09 13:27:44 +0200

    Free code camp drum chalange

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+const App = () => {
+
+  const [rengeVolum , setRendeVolume] = useState(0)
+
+  return(
+    <div id="drum-machine">
+      <div className="butoons-div">
+        <div>
+            <div className="drum-pad" id="Q">Q</div>
+            <div className="drum-pad" id="W">W</div>
+            <div className="drum-pad" id="E">E</div>
+        </div>
+        <div>
+            <div className="drum-pad" id="A">A</div>
+            <div className="drum-pad" id="S">S</div>
+            <div className="drum-pad" id="D">D</div>
+        </div>
+        <div>
+            <div className="drum-pad" id="Z">Z</div>
+            <div className="drum-pad" id="X">X</div>
+            <div className="drum-pad" id="C">C</div>
+        </div>
+      </div>
+      <div id="dispaly">
+        <div className="turn-div"></div>
+        <div className="result"></div>
+        <input type='range' max={1} min={0} step={0.01} value={rengeVolum} />
+      </div>
     </div>
-  );
+  )
 }
+
 
 export default App;
